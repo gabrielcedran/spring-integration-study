d71cf1b129707f2d54fccddd5bc9ec370aa0baa5
Gabriel Cedran <gabriel@getfuzey.com>
2022-07-25 19:37:59 +0100

    Bootstrap project

---
+++ src/main/java/br/com/cedran/springintegration/SpringintegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.cedran.springintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cedran/springintegration/SpringintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cedran.springintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
